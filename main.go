3e996ab27c51247033cc8fb15a6a518e318247f9
Neal Patel <nealpatel@google.com>
2025-05-05 19:09:11 -0400

    doc comments

--- main.go
+++ main.go
@@ -10,16 +10,17 @@ func main() {
 	println(oled.SetupDisplay())
 	time.Sleep(time.Second) // todo: remove if stable
 
-	frames := [][512]byte{oled.GopherBelly, oled.GopherDrink, oled.GopherGraduate}
+	// todo: consider "Save Memory" under https://tinygo.org/docs/guides/tips-n-tricks/
+	frames := [][]byte{oled.GopherBelly, oled.GopherDrink, oled.GopherGraduate}
 	for ever() {
 		for i := range frames {
-			oled.Draw(frames[i][:])
+			oled.Draw(frames[i]) // should not copy
 			time.Sleep(time.Second)
 		}
 	}
 }
 
-// ever is a small QoL function to ensure minimum polling rate.
+// ever is a small QoL function to ensure minimum sleep inside tight loop.
 func ever() bool {
 	time.Sleep(83 * time.Millisecond)
 	return true
